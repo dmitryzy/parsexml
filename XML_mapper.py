396d56f5f8aca1baa08f044617f493a0fd989f6d
Dmitry Zymai <dmitry-zy@yandex.ru>
2016-03-14 16:36:52 +0300

    Добавлены методы
    add_parse_objects, add_parse_tags,

--- XML_mapper.py
+++ XML_mapper.py
@@ -20,22 +20,47 @@ class XmlMmapper:
         Выполняется подключение файла-источника
         path Путь к файлу XML
         """
+        self.dom=dom.parse(path)
+        #
+        self.dom.normalize()
+        self.tree = etree.parse(path)
+        self.root = self.tree.getroot()
+        self.results_of_element=[]
+        self.filtred_results_of_element=[]
+        self.name_parse_obj={}
+        self.model_tags={}
+        self.model_tag_type=['value','link','list','text']
 
     def add_parse_objects(self,element_name):
         """Метод для добавления интересующих элементов
         element_name - имя объекта
         """
+        self.name_parse_obj[element_name]=self.tree.findall('*'+element_name)
+        self.find(element_name)
+
+    def add_parse_tags(self,model_tag_name, model_tag_type):
+        """Метод для добавления интересующих тегов
+        model_tag_name - название тега
+        model_tag_type - тип
+        """
+        if model_tag_type not in self.model_tag_type:
+            model_tag_type=None
+        self.model_tags[model_tag_name]=model_tag_type
 
     def is_parse_tag(self,tag_name):
         """Метод проверки наличия тега в списке
         Используется для фильтра в методе __find()
         tag_name - Имя тега
         """
+        res=True if ((tag_name in self.model_tags)or(tag_name in self.name_parse_obj)) else False
+        return res
 
     def tojson(self,file_json_name):
         """ Метод для преобразования результатов работы парсера в json формат
         file_json_name - имя файла
         """
+        with open(file_json_name+'.json', 'w'):
+            write(json.dumps(self.filtred_results_of_element, sort_keys=True, indent=4))
 
     def tag_filter(self):
         """Метод для фильтрации тегов
