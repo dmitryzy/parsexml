b7be52e3aa8165a52779dc4754b4e9aedf825ef4
Dmitry Zymai <dmitry-zy@yandex.ru>
2016-03-14 17:21:57 +0300

    Дополнения
    Демонстрация, тесты

--- main.py
+++ main.py
@@ -10,10 +10,12 @@
 #-------------------------------------------------------------------------------
 import XML_mapper
 def main():
+    #Подключаем модуль
     mapper=XML_mapper.XmlMmapper('test.xml')
+    # Указываем объект
     mapper.add_parse_objects('event')
-    #
-    #mapper.add_parse_tags('title','value')
+    #Указываем теги, которые следует учитывать в модели
+    mapper.add_parse_tags('title','value')
     mapper.add_parse_tags('runtime','value')
     mapper.add_parse_tags('age_restricted','value')
     mapper.add_parse_tags('tags','list')
@@ -22,11 +24,17 @@ def main():
     mapper.add_parse_tags('tag','value')
     mapper.add_parse_tags('description','value')
     mapper.add_parse_tags('stage_theatre','value')
+    #Выполняем поиск
     mapper.find('event')
+    #Выводим на печать результат
     print(mapper.results_of_element[1])
+    #Импорт в json для дальнейшей работы с моделью
+    #Задаем имя файла 'test'
     mapper.tojson('test')
-    print(mapper.is_parse_tag('title'))
+    #Фильтруем теги, оставляя только заданные
     mapper.tag_filter('model')
+    #Выводим на печать результат фильтрации
+    print(mapper.results_of_element[1])
 
 if __name__ == '__main__':
     main()
